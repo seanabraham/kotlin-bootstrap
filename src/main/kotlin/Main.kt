ce8a8fa49f4758e20249f5f51ea6f99fa89f203e
Sean Abraham <sean.abraham@airbnb.com>
2016-05-23 00:59:59 -0700

    Basic bootstrap kotlin gradle app

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello, world! Setting up java/gradle projects blows!")
+        println("Fortunately I have this bootstrap project!")
+    }
+}
